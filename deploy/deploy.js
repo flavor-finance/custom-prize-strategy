13db426cebaf101f8c78410da00d04d569f9e888
alex <alex@eristica.com>
2020-10-18 18:45:28 +0400

    Deploy fix

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,23 +1,34 @@
-const debug = require('debug')('custom-prize-strategy:deploy')
+const debug = require("debug")("custom-prize-strategy:deploy");
+const { ethers } = require("@nomiclabs/buidler");
 
-module.exports = async (buidler) => {
-  const { getNamedAccounts, deployments } = buidler
-  const { deploy } = deployments
-  const { deployer } = await getNamedAccounts()
+async function main() {
+  const [deployer] = await ethers.getSigners();
 
-  debug("\n  Deploying FlavorProxyFactory...")
-  const flavorProxyFactoryResult = await deploy("FlavorProxyFactory", {
-    from: deployer,
-    skipIfAlreadyDeployed: true
-  })
+  console.log(
+    "Deploying contracts with the account:",
+    await deployer.getAddress()
+  );
 
-  debug("\n Deploying FlavorBuilder...")
-  const flavorBuilderResult = await deploy("FlavorBuilder", {
-    args: [
-      flavorProxyFactoryResult.address
-    ],
-    from: deployer,
-    skipIfAlreadyDeployed: true
-  })
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-};
+  debug("\n  Deploying FlavorProxyFactory...");
+  const FlavorProxyFactory = await ethers.getContractFactory(
+    "FlavorProxyFactory"
+  );
+  const flavorProxy = await FlavorProxyFactory.deploy();
+  await flavorProxy.deployed();
+  console.log("FlavorProxyFactory address:", flavorProxy.address);
+
+  debug("\n  Deploying Flavor Builder...");
+  const FlavorBuilder = await ethers.getContractFactory("FlavorBuilder");
+  const flavorBuilder = await FlavorBuilder.deploy(flavorProxy.address);
+  await flavorBuilder.deployed();
+  console.log("FlavorBuilder address:", flavorBuilder.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
