088749bef83722be60db52f72d2244daf0168f64
alex <alex@eristica.com>
2020-10-20 19:22:37 +0400

    deploy test

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -4,6 +4,9 @@ const { ethers } = require("@nomiclabs/buidler");
 async function main() {
   const [deployer] = await ethers.getSigners();
 
+  ///Prize Pool address creaated in the Builder: https://builder.pooltogether.com/
+  const prizePoolAddress = "0x5259fcC7e86E8e5997AcdDA4E09Fd326500c44A1";
+
   console.log(
     "Deploying contracts with the account:",
     await deployer.getAddress()
@@ -15,15 +18,55 @@ async function main() {
   const FlavorProxyFactory = await ethers.getContractFactory(
     "FlavorProxyFactory"
   );
-  const flavorProxy = await FlavorProxyFactory.deploy();
-  await flavorProxy.deployed();
-  console.log("FlavorProxyFactory address:", flavorProxy.address);
+  const flavorProxyInst = await FlavorProxyFactory.deploy();
+  await flavorProxyInst.deployed();
+  saveContractAddress("FlavorProxyFactory", flavorProxyInst.address);
+  console.log("FlavorProxyFactory address:", flavorProxyInst.address);
 
-  debug("\n  Deploying Flavor Builder...");
+  debug("\n  Deploying FlavorBuilder...");
   const FlavorBuilder = await ethers.getContractFactory("FlavorBuilder");
-  const flavorBuilder = await FlavorBuilder.deploy(flavorProxy.address);
-  await flavorBuilder.deployed();
-  console.log("FlavorBuilder address:", flavorBuilder.address);
+  const flavorBuilderInst = await FlavorBuilder.deploy(flavorProxyInst.address);
+  await flavorBuilderInst.deployed();
+  saveContractAddress("FlavorBuilder", flavorBuilderInst.address);
+  console.log("FlavorBuilder address:", flavorBuilderInst.address);
+
+  const tx = deployer.sendTransaction({
+    to: flavorBuilderInst.address,
+    value: ethers.utils.parseEther("0.1"),
+  });
+
+  await deployer.waitForTransaction(tx, 1);
+  console.log("send tx:", tx);
+
+  console.log("Starting flavorBuilder.createFlavorStrategy");
+  await flavorBuilderInst.createFlavorStrategy(prizePoolAddress);
+  console.log(
+    "FlavorBuilder created Flavoer Strategy at the address:",
+    prizePoolAddress
+  );
+
+  //const erc20_rw = new ethers.Contract(address, abi, signer)
+  // const prizePoolContract = await ethers.Contract(
+  //   prizePoolAddress,
+
+  // );
+
+  console.log("adddress: ", prizePoolContract.address);
+  //PrizePoolContract.setPrizeStrategy(strategyAddress)
+}
+
+function saveContractAddress(name, address) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../artifacts/address";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify({ address: address }, undefined, 2)
+  );
 }
 
 main()
---
+++ deploy/deploy_test.js
@@ -0,0 +1,69 @@
+const debug = require("debug")("custom-prize-strategy:deploy");
+const { ethers } = require("@nomiclabs/buidler");
+const abiFlavorBuilder = require("../artifacts/FlavorBuilder.json").abi;
+const abiFlavorProxyFactory = require("../artifacts/FlavorProxyFactory.json").abi;
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  ///Prize Pool address creaated in the Builder: https://builder.pooltogether.com/
+  const prizePoolAddress = "0x5259fcC7e86E8e5997AcdDA4E09Fd326500c44A1";
+  let flavorBuilderAddress = await findAddress("FlavorBuilder");
+  let flavorProxyFactoryAddress = await findAddress("FlavorProxyFactory");
+
+  console.log("Connecting contracts with the account:", await deployer.getAddress());
+
+  console.log("Account balance:", ethers.utils.formatEther(await deployer.getBalance()));
+
+  debug(`\n  Connecting to FlavorProxyFactory (${flavorProxyFactoryAddress})...`);
+  const flavorProxy = await new ethers.Contract(flavorProxyFactoryAddress, abiFlavorProxyFactory, deployer);
+
+  console.log(
+    "FlavorProxyFactory address:",
+    flavorProxy.address,
+    "\n Contract balance:",
+    ethers.utils.formatEther(await ethers.provider.getBalance(flavorProxy.address))
+  );
+
+  debug(`\n  Connecting to FlavorBuilder (${flavorBuilderAddress})...`);
+  const flavorBuilder = await new ethers.Contract(flavorBuilderAddress, abiFlavorBuilder, deployer);
+  console.log(
+    "FlavorBuilder address:",
+    flavorBuilder.address,
+    "\n Contract balance:",
+    ethers.utils.formatEther(await ethers.provider.getBalance(flavorProxy.address))
+  );
+
+  console.log("Sending 0.1 ETH to flavorBuilder address:", flavorBuilder.address);
+  const tx = await deployer.sendTransaction({
+    to: flavorBuilder.address,
+    value: ethers.utils.parseEther("0.1"),
+  });
+
+  //await (await tacoToken.transfer(crowdsaleContract.address, CIRCULATING_SUPPLY)).wait(1);
+  console.log("send tx:", tx);
+
+  console.log("Starting flavorBuilder.createFlavorStrategy");
+  await flavorBuilder.createFlavorStrategy(prizePoolAddress);
+  console.log("FlavorBuilder created Flavoer Strategy at the address:", prizePoolAddress);
+  console.log("FlavorBuilder.createFlavorStrategy done");
+}
+
+async function findAddress(name) {
+  const fs = require("fs");
+  const addressesFile = __dirname + `/../artifacts/address/${name}.json`;
+  if (!fs.existsSync(addressesFile)) {
+    console.error(`You need to deploy your ${name} contract first`);
+    return;
+  }
+  const addressJson = fs.readFileSync(addressesFile);
+  const address = JSON.parse(addressJson);
+  return address.address;
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
