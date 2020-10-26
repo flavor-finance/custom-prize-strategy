7d8a8980c20ebe910c36b219b7353c8f84c13117
alex <alex@eristica.com>
2020-10-26 19:22:41 +0400

    Migration fix

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,8 +1,5 @@
 const FlavorBuilder = artifacts.require("FlavorBuilder");
 const FlavorProxyFactory = artifacts.require("FlavorProxyFactory");
-const FlavorStrategy = artifacts.require("FlavorStrategy");
-//const Web3 = require('web3');
-//let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
 module.exports = async function (deployer, network, accounts) {
   // Deploy Flavor Proxy Factory
